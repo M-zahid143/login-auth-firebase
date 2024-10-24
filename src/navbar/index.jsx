347ed2a4195bc2e2e28aa8e0261afd4ecaec1bf1
M-zahid143 <zahidwaran@gmail.com>
2024-10-24 12:31:22 +0500

    add navbar

---
+++ src/navbar/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <div>
+            <h1>Logo</h1>
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Product</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div>
+            <button>
+                Login
+            </button>
+            <button>
+                SignUp
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default index
